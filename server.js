817aa6aa53fc2389f705bc96f2a2c00490ed4d14
Nar Anthony Esguerra <naresguerra02@gmail.com>
2025-05-17 11:45:36 +0800

    Server Setup Completed

---
+++ server.js
@@ -0,0 +1,18 @@
+const app = require('./app');
+const sequelize = require('./config/db');
+require('dotenv').config();
+const Post = require('./models/post');
+
+const PORT = process.env.PORT || 3000;
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () =>
+      console.log('Server is running on http://localhost:${PORT}')
+    );
+  } catch (err) {
+    console.error('Database connection error:', err);
+  }
+})();
